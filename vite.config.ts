7b2a9f3f6a89acfa2dd057fc58ecb1f6e8e9aae4
Andreas Söderlund <ciscoheat@gmail.com>
2026-08-26 21:25:04 +0200

    Avoid Sass legacy API warning

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
 import adapter from '@sveltejs/adapter-auto';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { sveltePreprocess } from 'svelte-preprocess';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: ['legacy-js-api']
+      }
+    }
+  },
   plugins: [
     sveltekit({
       adapter: adapter(),
-      preprocess: sveltePreprocess(),
+      preprocess: vitePreprocess(),
       csrf: {
         trustedOrigins: ['*']
       }
