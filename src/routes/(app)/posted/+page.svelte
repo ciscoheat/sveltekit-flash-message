251cca5e2603b4c448f4c1898026f84a45dc47bb
Andreas Söderlund <ciscoheat@gmail.com>
2026-08-26 09:11:02 +0200

    Linter

--- src/routes/(app)/posted/+page.svelte
+++ src/routes/(app)/posted/+page.svelte
@@ -1,29 +1,29 @@
 <script lang="ts">
 </script>
 
-	<aside class="interactions"></aside>
+<aside class="interactions"></aside>
 
 <article>
-	<h1>After POST page</h1>
-	<p>This is where a POST should redirect. <a href="/">Back</a></p>
-	<p>
-		Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Vivamus suscipit tortor
-		eget felis porttitor volutpat. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet
-		dui.
-	</p>
+  <h1>After POST page</h1>
+  <p>This is where a POST should redirect. <a href="/">Back</a></p>
+  <p>
+    Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Vivamus suscipit tortor
+    eget felis porttitor volutpat. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet
+    dui.
+  </p>
 </article>
 
 <style lang="scss">
-	@use '../mixins.scss' as *;
+  @use '../mixins.scss' as *;
 
-	article {
-		@include stack-vertical;
+  article {
+    @include stack-vertical;
 
-		padding: 2rem 4rem;
+    padding: 2rem 4rem;
 
-		background-color: $white;
+    background-color: $white;
 
-		border: 1px solid rgb(224, 224, 224);
-		border-radius: 8px;
-	}
+    border: 1px solid rgb(224, 224, 224);
+    border-radius: 8px;
+  }
 </style>
--- svelte.config.js
+++ svelte.config.js
@@ -3,17 +3,17 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter(),
-		csrf: {
-			// For ngrok to work
-			trustedOrigins: ['*']
-		}
-	}
+  kit: {
+    adapter: adapter(),
+    csrf: {
+      // For ngrok to work
+      trustedOrigins: ['*']
+    }
+  }
 };
 
 export default config;
